7a0b723265ad6581ccc7153f93f1f3a032976544
themaxta <themaxta@gmail.com>
2015-11-26 02:25:31 -0700

    first commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,60 @@
+class PostsController < ApplicationController
+	before_action :find_post, only: [:show, :edit, :update, :destroy]
+	#this above line is just so we can write this out faster..
+	#the above methods will use the find_post method, so this just helps it happen faster
+
+	def index 
+		@posts = Post.all.order("created_at DESC")
+	end
+
+
+	def show 
+	end
+	def new 
+		@post = current_user.posts.build
+
+	end
+
+	def create
+		@post = current_user.posts.build(post_params)
+
+			if @post.save
+				redirect_to @post
+			else
+				render 'new'
+			end
+	end
+	def edit
+
+	end
+
+
+	def update
+		if @post.update(post_params)
+			redirect_to @post
+		else 
+			render 'edit'
+		end
+
+
+	end
+	def delete
+	end
+	
+	def destroy
+		@post.destroy
+		redirect_to root_path
+	end
+
+ private
+
+ 	def find_post
+		@post = Post.find(params[:id])
+
+ 	end
+
+	def post_params
+		params.require(:post).permit(:title, :content)
+
+	end
+end
